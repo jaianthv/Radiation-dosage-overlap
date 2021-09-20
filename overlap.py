1c8f0817682a4d783422b952ddb3a10034162870
Jaianthv <43813831+jaianthv@users.noreply.github.com>
2021-09-20 11:39:48 +0200

    Update overlap.py

--- overlap.py
+++ overlap.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
+'''
 R = 31E-9
 
 multi_fac = []
@@ -38,8 +38,38 @@ for i in range(1,50):
 #x =  math.acos( 1/2 * R *(2* d**2)**(1/2) )
     #print (x/(3.14*R*R))
 
+'''  
+   
+ 
+def find_overlap(R, d):
+    N_HV = round(2*(R/d))
+    Area = 0
+    for i in range(1,N_HV):
+        for j in range(1,N_HV):
+            dij = sqrt((i*d)**2+(j*d)**2)
+            Area = Area + 2 * R**2 * math.acos(dij/(2*R))
+    return Area, dij
+   
+ 
 
-plt.plot(y,multi_fac, "o--", color = 'black', linewidth= 2)
+def cal_all(R,No):
+   #R = 60 nm
+   #d = 1 nm - 65 nm
+   overlap_area = []
+   ratio = []
+    for i in range(1,No):
+        d = i * 1e-9; 
+        overlap = find_overlap(R,d)
+        overlap_area.append(overlap[0])
+        ratio.append(overlap[1])
+        
+      
+    return ratio, overlap_area
+  
+x, multi_fac = cal_all(62e-9, 65)
+
+
+plt.plot(x,multi_fac, "o--", color = 'black', linewidth= 2)
 #plt.plot(Z_real,Z_imaginary,'o')
 #plt.xscale("log")
 #plt.yscale("log")
